e6891ad5c7fd056cee696bdbb11e0d77467a80e5
Francisco Rodrigues <f.co.victor@hotmail.com>
2018-09-17 11:14:44 -0300

    Relacionamento Many to Many

---
+++ src/main/java/com/francisco/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.francisco.cursomc.repositories;
+
+import com.francisco.cursomc.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}

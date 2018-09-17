441e08da9fe959628b80b2b62e48a5e799d94580
Francisco Victor Rodrigues de Sousa <f.co.victor@hotmail.com>
2018-09-17 17:31:28 -0300

    Exception Handler

--- src/main/java/com/francisco/cursomc/model/Produto.java
+++ src/main/java/com/francisco/cursomc/model/Produto.java
@@ -1,5 +1,7 @@
 package com.francisco.cursomc.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ public class Produto implements Serializable {
     private String nome;
     private Double preco;
 
+    @JsonBackReference
     @ManyToMany
     @JoinTable(name = "produto_categoria",
         joinColumns = @JoinColumn(name = "produto_id"),
--- src/main/java/com/francisco/cursomc/service/CategoriaService.java
+++ src/main/java/com/francisco/cursomc/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.francisco.cursomc.service;
 
 import com.francisco.cursomc.model.Categoria;
 import com.francisco.cursomc.repositories.CategoriaRepository;
+import com.francisco.cursomc.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,8 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = categoriaRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()
+        ));
     }
 }

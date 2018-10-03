19dc9d461a3352b6cad2bdce237a342e24b0ea8f
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2018-10-03 16:18:12 -0300

    Deletando clientes

--- src/main/java/com/francisco/cursomc/model/Cliente.java
+++ src/main/java/com/francisco/cursomc/model/Cliente.java
@@ -23,7 +23,7 @@ public class Cliente implements Serializable{
     private String cpfOuCnpj;
     private Integer tipoCliente;
 
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection

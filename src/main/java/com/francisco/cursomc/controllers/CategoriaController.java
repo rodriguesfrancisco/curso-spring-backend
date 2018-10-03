6e197bf7d6d5bc29d642eb2c6af12f75bcd93be9
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2018-10-03 11:20:27 -0300

    Pageano Categoria

--- src/main/java/com/francisco/cursomc/controllers/CategoriaController.java
+++ src/main/java/com/francisco/cursomc/controllers/CategoriaController.java
@@ -4,6 +4,7 @@ import com.francisco.cursomc.dto.CategoriaDTO;
 import com.francisco.cursomc.model.Categoria;
 import com.francisco.cursomc.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -54,5 +55,17 @@ public class CategoriaController {
         List<CategoriaDTO> listDto = list.stream().map(obj ->
                 new CategoriaDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        Page<Categoria> pageCat = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> pageDto = pageCat.map(obj -> new CategoriaDTO(obj));
+
+        return ResponseEntity.ok().body(pageDto);
     }
 }

71eb1e8d5f285052b3161e0d93c412764cc4198c
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2018-10-10 17:43:19 -0300

    Autorização JWT

---
+++ src/main/java/com/francisco/cursomc/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.francisco.cursomc.service;
+
+import com.francisco.cursomc.model.Cliente;
+import com.francisco.cursomc.repositories.ClienteRepository;
+import com.francisco.cursomc.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Cliente cli = clienteRepository.findByEmail(email);
+        if(cli == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+    }
+}

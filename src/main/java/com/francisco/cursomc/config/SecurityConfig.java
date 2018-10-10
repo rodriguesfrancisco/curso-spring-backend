473536cb05f3f172c3d4020a85cd8eb3c4bbe14d
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2018-10-10 18:17:07 -0300

    Terminando Autorização

--- src/main/java/com/francisco/cursomc/config/SecurityConfig.java
+++ src/main/java/com/francisco/cursomc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.francisco.cursomc.config;
 
 import com.francisco.cursomc.security.JWTAuthenticationFilter;
+import com.francisco.cursomc.security.JWTAuthorizationFilter;
 import com.francisco.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
